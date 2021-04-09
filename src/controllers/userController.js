132e35dfd03d3cde99e289995181cadd9ac96c45
Nath <nathywar2000@gmail.com>
2021-04-09 00:46:46 -0300

    login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,38 @@
 const {User}= require('../database/models/index');
+const {validationResult} = require('express-validator');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     login: function(req, res){
         res.render('pages/users/login');
     },
+    
+
+    userCheck: async function(req, res){
+        let user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if(user){
+            if(bcrypt.compareSync(req.body.password, user.password)){
+                return res.redirect('/');
+            } else {
+                return res.send('invalid password');
+            }
+        }
+        return res.send('email does not exist');
+
+    },
+
+
     register: function(req, res){
         res.render('pages/users/register');
     },
     registerStore: async function(req, res){
+        let errorRegister = validationResult(req);
+       if(errorRegister.isEmpty()){
         await User.create({
             firstName: req.body.firstName,
             lastName: req.body.lastName,
@@ -20,5 +45,8 @@ module.exports = {
         }).catch((error)=>{
             console.log(error.message);
         })
+       }else{
+        res.render('pages/users/login', {errorRegister: errorRegister.mapped()});
+       }
     }
 }
